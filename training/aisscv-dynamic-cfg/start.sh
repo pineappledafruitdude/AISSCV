7937e824e85f86e34b73fd10a3aed8922fa6fdda
pineappledafruitdude <59140117+pineappledafruitdude@users.noreply.github.com>
2021-07-08 18:43:51 +0200

    Fixed start script (added batch_size)

--- training/aisscv-dynamic-cfg/start.sh
+++ training/aisscv-dynamic-cfg/start.sh
@@ -43,4 +43,4 @@ do
 done
 (cd "$CURRENT_PATH" && git clone https://github.com/pineappledafruitdude/AISSCV.git aisscv)
 (cd "$CURRENT_PATH"aisscv/preprocessing/ &&
-python3.8 run_train.py -n="$NAME" -o "/aisscv/model" -c="$COLOR" -f="$FOLDS" -nbr_augment="$NBR_AUGMENTATIONS" -darknet "$DARKNET")
+python3.8 run_train.py -n="$NAME" -o "/aisscv/model" -c="$COLOR" -f="$FOLDS" -batch_size"$BATCH_SIZE" -nbr_augment="$NBR_AUGMENTATIONS" -darknet "$DARKNET")
